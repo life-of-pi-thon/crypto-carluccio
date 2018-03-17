769f4cba96b0694bbd98b27e74783fa2f28873b7
Carl Oliver <carl_oliver@live.co.uk>
2018-03-17 13:51:38 +0000

    - created interface for binance to place a test  order

---
+++ business_logic/fees.py
@@ -0,0 +1,10 @@
+class exchangeFees:
+    def __init__(self, withdrawal_percent, deposit_percent, trading_percent):
+        self.withdrawal_percent = withdrawal_percent
+        self.deposit_percent = deposit_percent
+        self.trading_percent = trading_percent
+
+# class gdax_fees(exchangeFees):
+#     def __init__(self, withdrawal_percent, deposit_percent, trading_percent):
+#         #to complete
+
